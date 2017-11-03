e2e9868e44e0f4b98aa33e0b769a7cab569d2baa
kokoax <yomo4808@gmail.com>
2017-11-03 19:11:54 +0900

    Add Redisに音楽データを格納する機能を追加

---
+++ lib/mpd_client/util/update_db.ex
@@ -0,0 +1,35 @@
+defmodule MpdClient.Util.UpdateDB do
+  use GenServer
+  import Logger
+  @musicdir "/home/kokoax/Music/"
+
+  def update_db do
+    {:ok, conn} = Redix.start_link(host: "localhost", port: 6379)
+
+    :util
+    |> GenServer.call({:list_all, "/"})
+    |> Enum.filter(&(&1.type == "file"))
+    |> Enum.each(fn(path) ->
+      {:ok, t} = (@musicdir <> path.data) |> Taglib.new()
+      t |> store_data(path.data, conn)
+    end)
+  end
+
+  def store_data(tag, path, conn) do
+    [
+      fn(tag) -> {path <> ":album",       tag |> Taglib.album()} end,
+      fn(tag) -> {path <> ":artist",      tag |> Taglib.artist()} end,
+      fn(tag) -> {path <> ":compilation", tag |> Taglib.compilation()} end,
+      fn(tag) -> {path <> ":disc",        tag |> Taglib.disc()} end,
+      fn(tag) -> {path <> ":duration",    tag |> Taglib.duration()} end,
+      fn(tag) -> {path <> ":genre",       tag |> Taglib.genre()} end,
+      fn(tag) -> {path <> ":title",       tag |> Taglib.title()} end,
+      fn(tag) -> {path <> ":track",       tag |> Taglib.track()} end,
+      fn(tag) -> {path <> ":year",        tag |> Taglib.year()} end,
+    ] |> Enum.each(fn(func) ->
+      {id, key} = tag |> func.()
+      conn |> Redix.command(["SET", id, key])
+    end)
+    :ok
+  end
+end
