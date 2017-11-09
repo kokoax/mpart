6ef47075296151b42ca49fb450c63b98ea9d8871
kokoax <yomo4808@gmail.com>
2017-11-09 20:02:34 +0900

    Fix Redisを使ってテストを行いたいため、ignoreを解除

---
+++ config/config.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+config :mpd_client,
+  api_token: "Last.fm API Token",
+  musicdir: "/home/kokoax/Music/",
+  redis_host: "localhost",
+  redis_port: 6379
+
+
+# config :logger, level: :info
+
